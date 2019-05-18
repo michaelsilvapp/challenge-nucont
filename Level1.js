8402ca4c59bb5f30f49db3e80e521ad2e0190015
Dev <desenvolvimento01@rocketon.app>
2019-05-18 12:41:10 -0300

    Level one completed

---
+++ Level1.js
@@ -0,0 +1,43 @@
+var fs = require("fs");
+
+const dictionary = {
+    0: 'classifier',
+    1: 'description',
+    2: 'openingBalance',
+    3: 'debit',
+    4: 'credit',
+    5: 'finalBalance'
+}
+
+const LevelOneChallenge = (input) => {
+    const allItems = input.split(/,|  |\n/).filter(item => item !== '')
+
+    const objects = []
+    let tempObj = {}
+    for (index in allItems) {
+        const keyDic = index % 6
+
+        tempObj[dictionary[keyDic]] = allItems[index].trim()
+
+        if (keyDic === 5) {
+            tempObj.parent = index - 11 >= 0 ? allItems[index - 11] : null
+
+            objects.push(tempObj)
+            tempObj = {}
+        }
+    }
+
+    return objects
+}
+
+fs.readFile("Level1.txt", (err, buf) => {
+    const input = buf.toString()
+
+    console.log('Input:\n', input, '\n')
+    
+    const output = LevelOneChallenge(input)
+
+    console.log('Output:\n', output, '\n')
+});
+
+
